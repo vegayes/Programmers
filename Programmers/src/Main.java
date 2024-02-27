a2e86c608df80895feabd871b81e1865183d4e30
vegayes <vegayes@naver.com>
2024-02-27 23:58:59 +0900

    open chatting

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -13,20 +13,44 @@ class Main {
 
 class Solution {
     public String[] solution(String[] record) {
-        String[] answer = {};
 
         Map<String,String > userNick = new HashMap<>();
+        int count = 0;
+        int index = 0;
+
 
         //레코드 돌리기
         for(int i = 0; i < record.length; i++){
             String[] oneRecord = record[i].split(" ");
 
-            if()
+            // 유저 아이디 마다 닉네임 저장
+            if(oneRecord[0].equals("Enter") && !userNick.containsKey(oneRecord[1])){
+                userNick.put(oneRecord[1], oneRecord[2]);
+            }else if (oneRecord[0].equals("Change")||oneRecord[0].equals("Enter")){ // 변경 시, nickName 변경
+                userNick.put(oneRecord[1], oneRecord[2]);
+            }
 
+            // 배열 값 확인
+            if(oneRecord[0].equals("Enter")||oneRecord[0].equals("Leave")){
+                count++;
+            }
         }
 
+        String[] answer = new String[count];
 
+        // 출력하기
+        for(int a = 0; a < record.length; a++){
+            String[] option = record[a].split(" ");
 
+            if(option[0].equals("Enter")){
+                answer[index] = userNick.get(option[1])+"님이 들어왔습니다.";
+                index++;
+            }else if(option[0].equals("Leave")){
+                answer[index] = userNick.get(option[1])+"님이 나갔습니다.";
+                index++;
+            }
+
+        }
 
         return answer;
     }
