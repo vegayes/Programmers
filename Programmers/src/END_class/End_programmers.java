b78b8eebdb63e1b08d047644ca889da7477a95d2
vegayes <vegayes@naver.com>
2024-03-05 19:00:54 +0900

    reset

--- Programmers/src/END_class/End_programmers.java
+++ Programmers/src/END_class/End_programmers.java
@@ -436,4 +436,45 @@ public class End_programmers {
             return answer;
         }
     }
+
+
+    class Solution_type_min {
+        public int solution(int k, int[] tangerine) {
+            int answer = 0;
+            // 서로 다른 종류의 수를 최소화 시키고자 함.
+
+            // 정렬 시킴
+
+            // 1,2,2,3,3,4,5,5
+            // 2,2,3,3,5,5,1,4
+
+            Map<Integer, Integer> tan = new HashMap<>();
+
+
+            for(int i = 0; i < tangerine.length; i++){
+                if(!tan.containsKey(tangerine[i])){
+                    tan.put(tangerine[i], 1);
+                }else{
+                    tan.put(tangerine[i],tan.get(tangerine[i])+1);
+                }
+            }
+
+            List<Integer> listKeySet = new ArrayList<>(tan.keySet());
+
+            Collections.sort(listKeySet, (value1, value2) -> (tan.get(value2).compareTo(tan.get(value1))));
+
+            for(int key : listKeySet) {
+                if( k > 0 ){
+                    answer++;
+
+                    k = k - tan.get(key);
+
+                }else{
+                    break;
+                }
+            }
+
+            return answer;
+        }
+    }
 }
