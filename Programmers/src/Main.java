66d2bfba3c1528fd3ada3220788c45ef5ddc5a56
vegayes <vegayes@naver.com>
2024-03-11 23:24:27 +0900

    ?

--- Programmers/src/Main.java
+++ Programmers/src/Main.java
@@ -7,24 +7,16 @@ class Main {
     public static void main(String[] args) {
         Solution sol = new Solution();
 //        int[] result = sol.solution(new String[]{"I -45", "I 653", "D 1", "I -642", "I 45", "I 97", "D 1", "D -1", "I 333"});
-       String result = sol.solution(new String[]{"Jane", "Kim"});
         System.out.println(result);
     }
 
 }
 
-class Solution {
-    public String solution(String[] seoul) {
-        String answer = "";
+class Solution{
 
-        for(int i = 0; i < seoul.length; i++){
 
-            if(seoul[i].equals("Kim")){
-                return "김서방은 "+i+"에 있다";
-            }
 
-        }
 
-        return answer;
-    }
+
+
 }
